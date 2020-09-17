b20e1e1edd221f4cbb6c0e684f5112bb449d075a
ngthh <saccarozo.29@gmail.com>
2020-09-17 08:07:22 +0700

    index teacher

--- QLHocSinh/app/__init__.py
+++ QLHocSinh/app/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 from flask_admin_subview import Subview
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:namlun218@localhost/studentmanagementdb?charset=utf8mb4"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:12345@localhost/studentmanagementdb?charset=utf8mb4"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:namlun218@localhost/studentmanagementdb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:12345@localhost/studentmanagementdb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 app.secret_key = "vji  fuehf uhroih uo31hro31yr3yro31e3y r oryog"
