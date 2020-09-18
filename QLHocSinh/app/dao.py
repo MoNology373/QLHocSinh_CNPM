4049c40e4f39be7b88e620c9a835955dee0f720a
Nam <tphuongnam01@gmail.com>
2020-09-18 15:24:55 +0700

    teacher's display and searching, database's data

--- QLHocSinh/app/dao.py
+++ QLHocSinh/app/dao.py
@@ -1,41 +1,5 @@
 import hashlib
-import json
-import os
-
-from app import app
 from app.models import User
-
-
-def read_categories():
-    with open(os.path.join(app.root_path, "data/categories.json"),
-              encoding="utf-8") as f:
-        return json.load(f)
-
-
-def read_product_by_id(product_id):
-    products = read_products()
-    for p in products:
-        if p["id"] == product_id:
-            return p
-
-    return None
-
-
-def read_products(category_id=0, keyword=None, from_price=None, to_price=None):
-    with open(os.path.join(app.root_path, "data/products.json"),
-              encoding="utf-8") as f:
-        products = json.load(f)
-
-        if category_id > 0:
-            products = [p for p in products if p["category_id"] == category_id]
-
-        if keyword:
-            products = [p for p in products if p["name"].lower().find(keyword.lower()) >= 0]
-
-        # if from_price and to_price:
-        #     products = [p for p in products if p["price"] >= float(from_price) and p["price"] <= float(to_price)]
-
-        return products
 
 
 def validate_user_admin(username, password):
@@ -57,4 +21,4 @@ def validate_user_teacher(username, password):
 
 
 if __name__ == "__main__":
-    print(read_products())
+    print()
--- QLHocSinh/app/main.py
+++ QLHocSinh/app/main.py
@@ -7,10 +7,69 @@ from app.models import *
 @app.route('/')
 @login_required
 def index():
-    studentList = Student.query.filter().all()
-    classList = Class.query.filter().all
-    return render_template("index.html", studentList=studentList, classList=classList)
+    # studentList = Student.query.filter().all()
+    studentList = Student.query.join(Class, Student.class_id == Class.class_id).add_columns(Student.student_id,
+                                                                                            Student.lastName,
+                                                                                            Student.firstName,
+                                                                                            Student.gender,
+                                                                                            Student.date_of_birth,
+                                                                                            Student.email,
+                                                                                            Student.address,
+                                                                                            Class.name)
+    # classList = Class.query.filter().all
+    return render_template("index.html", studentList=studentList)
 
+
+@app.route('/score-semester-1')
+@login_required
+def score_semester_1():
+    # Student: student_id, lastName, firstName
+    # Subject: subject_name
+    # Semester: semester_name
+    # Score: score_fifteen, score_period, score_final
+    # Class: name
+    # studentList = Student.query.join(Class, Student.class_id == Class.class_id) \
+    #     .add_columns(Student.student_id, Student.lastName, Student.firstName, Class.name)
+
+    scoreList = Score.query.join(Student).join(Class).join(Semester).join(Subject)\
+        .add_columns(Student.student_id, Student.lastName, Student.firstName,
+                     Class.name, Semester.semester_name, Subject.subject_name,
+                     Score.score_fifteen, Score.score_period, Score.score_final).filter(Semester.semester_name == "1")
+
+    # scoreListBySemester = Semester.query.join(scoreList, Semester.semester_id == Score.semester_id) \
+    #     .add_columns(Student.student_id, Student.lastName, Student.firstName, Semester.semester_name,
+    #                  Score.score_fifteen, Score.score_period, Score.score_final, Class.name)
+    # scoreListBySubject = Subject.query.join(scoreListBySemester, Subject.id == Score.subject_id) \
+    #     .add_column(Student.student_id, Student.lastName, Student.firstName, Semester.semester_name,
+    #                 Subject.subject_name,
+    #                 Score.score_fifteen, Score.score_period, Score.score_final, Class.name)
+    return render_template("score-semester-1.html", scoreList=scoreList)
+
+
+@app.route('/score-semester-2')
+@login_required
+def score_semester_2():
+    # Student: student_id, lastName, firstName
+    # Subject: subject_name
+    # Semester: semester_name
+    # Score: score_fifteen, score_period, score_final
+    # Class: name
+    # studentList = Student.query.join(Class, Student.class_id == Class.class_id) \
+    #     .add_columns(Student.student_id, Student.lastName, Student.firstName, Class.name)
+
+    scoreList = Score.query.join(Student).join(Class).join(Semester).join(Subject)\
+        .add_columns(Student.student_id, Student.lastName, Student.firstName,
+                     Class.name, Semester.semester_name, Subject.subject_name,
+                     Score.score_fifteen, Score.score_period, Score.score_final).filter(Semester.semester_name == "2")
+
+    # scoreListBySemester = Semester.query.join(scoreList, Semester.semester_id == Score.semester_id) \
+    #     .add_columns(Student.student_id, Student.lastName, Student.firstName, Semester.semester_name,
+    #                  Score.score_fifteen, Score.score_period, Score.score_final, Class.name)
+    # scoreListBySubject = Subject.query.join(scoreListBySemester, Subject.id == Score.subject_id) \
+    #     .add_column(Student.student_id, Student.lastName, Student.firstName, Semester.semester_name,
+    #                 Subject.subject_name,
+    #                 Score.score_fifteen, Score.score_period, Score.score_final, Class.name)
+    return render_template("score-semester-2.html", scoreList=scoreList)
 
 @login.user_loader
 def user_load(user_id):
@@ -49,7 +108,6 @@ def login_teacher():
         user = dao.validate_user_teacher(username=username, password=password)
         if user:
             login_user(user=user)
-            flash('Thành công')
             return redirect(url_for("index"))
     return render_template("login.html", err_msg=err_msg)
 
@@ -60,16 +118,10 @@ def login_teacher():
 #     return render_template("submit.html")
 #
 #
-@app.route('/product')
-@login_required
-def product():
-    return render_template("product.html")
-
-
-@app.route('/score')
-@login_required
-def score():
-    return render_template("score.html")
+# @app.route('/product')
+# @login_required
+# def product():
+#     return render_template("product.html")
 
 
 @app.route('/log-out')
